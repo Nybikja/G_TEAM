7493e764aff2a1d8a6ba165edb5aa07510d8ef93
Nazar Vashchyshyn <nazar.ua87@gmail.com>
2021-12-10 01:37:56 +0200

    uploaded final version

---
+++ src/main/java/com/company/spring/web/MainController.java
@@ -0,0 +1,22 @@
+package com.company.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+<<<<<<< Updated upstream
+		return "index";
+=======
+		return "index.html";
+>>>>>>> Stashed changes
+	}
+}
